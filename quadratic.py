3d0c03f34966ce39fcade1ce6aa113934d7b73b0
longwaytop <aliaksandr.isai@gmail.com>
2018-06-12 16:36:41 +0300

    low fix

--- quadratic.py
+++ quadratic.py
@@ -4,16 +4,16 @@ from math import sqrt
 def solve(a, b, c):
     d = b*b - 4*a*c
     if d < 0:
-        print("no solutions ")
+        print("No solution")
     elif d == 0:
         x = -b / (2*a)
         print("One solution " + str(x))
     elif d > 0:
-        x1 = (-b + (sqrt(d)) / (2*a))
-        x2 = (-b - (sqrt(d)) / (2*a))
-        print("Two solutions " + str(x1) + "end " + str(x2))
+        x1 = (-b + sqrt(d)) / (2*a)
+        x2 = (-b - sqrt(d)) / (2*a)
+        print("Two solutions " + str(x1) + str(x2))
     else:
-        print("LOL_KEK_CHEBUREK!!!")
+        print("bla_bla_bla")
 
 
 solve(1, 1, 1)
