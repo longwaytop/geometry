fb8ace3bc59e26b7b6fdd00e3120dcc1d82de68f
longwaytop <aliaksandr.isai@gmail.com>
2018-06-15 17:25:02 +0300

    transfer of rules for comparing objects: LAMBDA

--- geometry.py
+++ geometry.py
@@ -3,15 +3,7 @@ from geom2d.point import *
 l1 = [Point(2, 1), Point(0, 0), Point(1, 2)]
 
 
-def x(p):
-    return p.x
-
-
-def y(p):
-    return p.y
-
-
-l2 = sorted(l1, key=y)
+l2 = sorted(l1, key=lambda p: p.x)
 
 print(l1)
 print(l2)
