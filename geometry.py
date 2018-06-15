b2b1fec9e87109951277be560b9fad10d848c779
longwaytop <aliaksandr.isai@gmail.com>
2018-06-15 19:19:12 +0300

    filter function

--- geometry.py
+++ geometry.py
@@ -2,7 +2,7 @@ from geom2d.point import *
 
 l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
 
-l2 = list(map(lambda p: Point(p.x, -p.y), l))
+l2 = list(filter(lambda p: p.x > 0, l))
 
 print(l)
 print(l2)
