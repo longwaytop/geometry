fe158cbdd5a1e575576b67eca3f663900dcc0d7d
longwaytop <aliaksandr.isai@gmail.com>
2018-06-15 17:27:15 +0300

    sorting point objects from the center of coordinate

--- geometry.py
+++ geometry.py
@@ -1,9 +1,9 @@
 from geom2d.point import *
 
-l1 = [Point(2, 1), Point(0, 0), Point(1, 2)]
+l1 = [Point(3, 1), Point(0, 0), Point(1, 2)]
 
 
-l2 = sorted(l1, key=lambda p: p.x)
+l2 = sorted(l1, key=lambda p: p.distance(Point(0, 0)))
 
 print(l1)
 print(l2)
