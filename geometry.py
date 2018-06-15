0980688fa84bca8c509957bd23d4de0bf3cbe773
longwaytop <aliaksandr.isai@gmail.com>
2018-06-15 15:59:48 +0300

    list sorting: l2 list got sorted by ascending of y coordinate

--- geometry.py
+++ geometry.py
@@ -1,7 +1,6 @@
 from geom2d.point import *
 
 l1 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-# l2 = [Point(0, 0), Point(1, 2), Point(2, 1)]
-l2 = list(l1)
-l2[0] = Point(0, 0)
-print(l1 == l2)
+
+l2 = sorted(l1)
+print(l1, l2)
